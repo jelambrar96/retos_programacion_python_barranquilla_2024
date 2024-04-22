8c33dfa27d49bb4184a2ad8661885f03f92939eb
roxi0103 <167358978+roxi0103@users.noreply.github.com>
2024-04-22 21:22:03 +0000

    roxi0103_reto_02

--- retos/reto_02/soluciones/roxi0103.py
+++ retos/reto_02/soluciones/roxi0103.py
@@ -1,16 +1,26 @@
 """
 escribe informacion de tu codigo aqui. 
 """
-
-def conteo_421(n: int):
-    """
-    documenta tu funcion aqui
-    """
-    pass
-
-
-if __name__ == '__main__':
-    """
-    escribe lo que quieras que tu codigo haga cuando se llame directamente
-    """
-    pass
+def conteo_421(numero):
+    contador=0
+    while numero>1:
+        if numero%2==0:
+            numero//=2
+            print("el nuevo valor es:",numero)
+            contador+=1
+            print("contador",contador)
+        else:
+            numero*=3
+            numero+=1
+            print("el nuevo valor es:",numero)
+            contador+=1
+            print("contador", contador)
+    return contador
+    
+    
+numero=int(input("digite un numero entero:"))
+print("el numero es:", numero,"y el numero de veces que se repite el proceso es", conteo_421(numero))
+git add ruta/archivo/c
+o
+git commit -m NombreUsusarioGithub_reto_02
+git push origin NombreUsusarioGithub_reto_02
