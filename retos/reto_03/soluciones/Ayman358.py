16e159084b3e8dc19366ebaecfd35d6d75b54e52
Ayman358 <167446166+Ayman358@users.noreply.github.com>
2024-04-25 16:35:37 +0000

    corecciones

--- retos/reto_03/soluciones/Ayman358.py
+++ retos/reto_03/soluciones/Ayman358.py
@@ -10,12 +10,25 @@ def fizzbuzz(start=1, end=101):
 
     rango = range(start, end, step)
     for i in rango:
-        print(i)
+        # print(i)
+        # if i=(multiplo de 3 reemplazar por el valor "fizz")
+        if i% 3==0 and i % 5== 0:
+            print ("fizzbuzz")         
+        elif i% 3==0 : 
+            print  ("fizz")
+        elif i % 5 == 0:
+            print("buzz")
+        # if i=(multiplo de 5 remplazar por el valor "buzz")
+        # if i=(multiplo de 3 y 5 remplazar por el valor "fizzbuzz")
+        else:
+            print(i)
+
 
 
 
 if __name__ == "__main__":
     print("prueba 1")
     fizzbuzz(1, 20)
+    print()
     print("prueba 2")
     fizzbuzz(20, 1)
