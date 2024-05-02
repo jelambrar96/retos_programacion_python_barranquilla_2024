75cc7b45963a5d0f78dee64d818d3f951ccc00e5
roxi0103 <167358978+roxi0103@users.noreply.github.com>
2024-05-02 16:04:50 +0000

    roxi0103.py

--- retos/reto_05/soluciones/roxi0103.py
+++ retos/reto_05/soluciones/roxi0103.py
@@ -30,21 +30,21 @@ def fahrenheit_to_kelvin(temp):
 
 if __name__ == '__main__':
 
-g= float(input("Digite la tempratura en celcius para convertir a fahrenheit: " ))
-print("La tempreratura",g,"°", "es equivalente a", celcius_to_fahrenheit(g), "°F" )
+    g= float(input("Digite la tempratura en celcius para convertir a fahrenheit: " ))
+    print("La tempreratura",g,"°", "es equivalente a", celcius_to_fahrenheit(g), "°F" )
 
-k= float(input("Digite la tempratura en celcius para convertir a kelvin: " ))
-print("La tempreratura",k,"°", "es equivalente a", celcius_to_kelvin(k), "K" )
+    k= float(input("Digite la tempratura en celcius para convertir a kelvin: " ))
+    print("La tempreratura",k,"°", "es equivalente a", celcius_to_kelvin(k), "K" )
 
-k2=float(input("Digite la temperatura en kelvin para convertir a fahrenheit: " ))
-print("La tempreratura",k2,"K", "es equivalente a", kelvin_to_fahrenheit(k2), "°F" )
+    k2=float(input("Digite la temperatura en kelvin para convertir a fahrenheit: " ))
+    print("La tempreratura",k2,"K", "es equivalente a", kelvin_to_fahrenheit(k2), "°F" )
 
-j=float(input("Digite la temperatura en kelvin para convertir a celcius: " ))
-print("La tempreratura",j,"K", "es equivalente a", kelvin_to_celcius(j), "°C" )
+    j=float(input("Digite la temperatura en kelvin para convertir a celcius: " ))
+    print("La tempreratura",j,"K", "es equivalente a", kelvin_to_celcius(j), "°C" )
 
-h=float(input("Digite la temperatura en fahrenheit para convertir a celcius: " ))
-print("La tempreratura",h,"°F", "es equivalente a", fahrenheit_to_celcius(h), "°C" )
+    h=float(input("Digite la temperatura en fahrenheit para convertir a celcius: " ))
+    print("La tempreratura",h,"°F", "es equivalente a", fahrenheit_to_celcius(h), "°C" )
 
-r=float(input("Digite la temperatura en fahrenheit para convertir a kelvin: " ))
-print("La tempreratura",r,"°F", "es equivalente a", fahrenheit_to_kelvin(r), "K" )
+    r=float(input("Digite la temperatura en fahrenheit para convertir a kelvin: " ))
+    print("La tempreratura",r,"°F", "es equivalente a", fahrenheit_to_kelvin(r), "K" )
 pass
