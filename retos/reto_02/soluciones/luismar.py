ff38a7c93c93d6cb63d84322491e5eb09b099751
Luismar <luis.martinezco@cecar.edu.co>
2024-05-04 21:26:21 -0500

    reto2

---
+++ retos/reto_02/soluciones/luismar.py
@@ -0,0 +1,14 @@
+def conteo_421(num):
+  cont = 0
+  while num > 1:
+    if num % 2 == 0:
+      num //= 2
+      cont += 1
+    else:
+      num *= 3
+      num += 1
+      cont += 1
+  return cont
+
+num=int(input("Ingrese su numero"))
+print(conteo_421(num))
