b23a8592916a0ee5bfcdc8eb485978a0138b3fea
yunver10 <emprededor4985@gmail.com>
2024-05-06 11:37:29 -0500

    reto 5 j hernendez

---
+++ retos/reto_05/soluciones/jhernadez1130.py
@@ -0,0 +1,16 @@
+def celcius_to_fahrenhit(temp):
+    f= (temp*9/5)+ 32
+    return f
+
+def celcius_to_kelvin(temp):
+    k=temp+273
+    return k
+
+def kelvin_to_fahrenhit(temp):
+    c=temp-273
+    return c
+
+def fahranhit_to_kelvin(temp):
+    Y=(temp-32) *(579) +273
+    return Y
+
